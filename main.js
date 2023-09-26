0901d3e9c6c004322e0135c1918ff58d29a59ee8
mayahilbert <35238760+mayahilbert@users.noreply.github.com>
2023-09-26 19:22:00 -0400

    demo of slider

--- main.js
+++ main.js
@@ -1,65 +1,18 @@
-var colors = new Array([39, 135, 189], [246, 94, 234], [137, 155, 237]);
-var colors2 = new Array([3, 13, 18], [255, 255, 255], [217, 215, 237]);
+const vidContainers = document.querySelectorAll('.vimeo-vid');
+console.log("playvideo");
+vidContainers.forEach(el => el.addEventListener('click', event => {  
+  playVideo(el);
+}));
 
-var step = 0;
-//color table indices for:
-// current color left
-// next color left
-// current color right
-// next color right
-var colorIndices = [0, 1];
+function playVideo(thisVid) {
+  var thisVideo = thisVid;
 
-//transition speed
-var gradientSpeed = 0.001;
+  var playButton = thisVideo.querySelectorAll('.playbutton')[0];
+  var player = thisVideo.querySelectorAll('iframe')[0];
+  var vimeoplayer = new Vimeo.Player(player);
 
-function updateGradient() {
-  var c0_0 = colors[colorIndices[0]];
-  var c0_1 = colors[colorIndices[1]];
-  var c0_2 = colors2[colorIndices[0]];
-  var c0_3 = colors2[colorIndices[1]];
+  $(playButton).addClass('hide');
+  $(thisVideo).addClass('show');
+  vimeoplayer.play();
 
-  var istep = 1 - step;
-  var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-  var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-  var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-
-  var color1 = "#" + ((r1 << 16) | (g1 << 8) | b1).toString(16);
-
-  var r2 = Math.round(istep * c0_2[0] + step * c0_3[0]);
-  var g2 = Math.round(istep * c0_2[1] + step * c0_3[1]);
-  var b2 = Math.round(istep * c0_2[2] + step * c0_3[2]);
-  var color2 = "#" + ((r2 << 16) | (g2 << 8) | b2).toString(16);
-
-  $(".gradient-animate").css({
-    background:
-      "-webkit-radial-gradient(center, circle contain, " +
-      color1 +
-      ", transparent 100%)",
-  });
-  $(".gradient-animate-2").css({
-    background:
-      "-webkit-radial-gradient(center, circle contain, " +
-      color2 +
-      ", transparent 100%)",
-  });
-  step += gradientSpeed;
-  if (step >= 1) {
-    step %= 1;
-    colorIndices[0] = colorIndices[1];
-
-    //pick two new target color indices
-    //do not pick the same as the current one
-    colorIndices[1] =
-      (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) %
-      colors.length;
-  }
-}
-
-setInterval(updateGradient, 10);
-
-// Add a click handler to all dropdowns on the page
-for (var e of document.querySelectorAll(".dropdown button")) {
-  e.addEventListener("click", function (evt) {
-    evt.target.parentElement.classList.toggle("open");
-  });
 }
